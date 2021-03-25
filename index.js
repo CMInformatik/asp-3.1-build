c16a7baf9290c5dd5c11730d99157db37c102e0b
Milan Bursac <milan.bursac@cmiag.ch>
2021-03-25 15:21:29 +0100

    feat(js action)

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ async function logInDockerRegistry() {
     let password = core.getInput('docker-password');
     let username = core.getInput('docker-username');
 
-    await exec.exec(`docker login --username "${username}" --password "${password}"`);
+    await exec.exec(`winpty docker login --username "${username}" --password "${password}"`);
 }
 
 async function setUpDockerBuildX() {
