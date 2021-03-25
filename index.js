bc61ca531036fc5ed1c1b48a623b3600af41bced
Milan Bursac <milan.bursac@cmiag.ch>
2021-03-25 15:18:27 +0100

    feat(js action)

--- index.js
+++ index.js
@@ -4,13 +4,56 @@ const exec = require('@actions/exec');
 
 async function run() {
     try {
-        console.log('Add nuget.config');
-        await exec.exec('dotnet new nugetconfig -o /tmp');
+        console.log('Setting variables');
+        const myGetNuGetSource = core.getInput('myget-pre-auth-url');
 
-        await exec.exec('echo test');
+        await addNugetConfig();
+        await ensureMyGetNuGetSource(myGetNuGetSource);
+        await prepare();
+        await setUpDockerBuildX();
+        await logInDockerRegistry();
+
     } catch (error) {
         core.setFailed(error.message);
     }
-  }
+}
 
-  run().then(_ => {});
+async function logInDockerRegistry() {
+    let password = core.getInput('docker-password');
+    let username = core.getInput('docker-username');
+
+    await exec.exec(`docker login --username "${username}" --password "${password}"`);
+}
+
+async function setUpDockerBuildX() {
+    console.log('Set up docker buildx');
+    await exec.exec('docker buildx install');
+}
+
+async function prepare() {
+    let repositoryName = core.getInput('app-name').toLowerCase();
+    let dockerImage = `registry.cmicloud.ch:4443/${repositoryName}`;
+    let version = 'edge';
+
+    let tags = `-t ${dockerImage}:${version}`;
+    if(github.context.eventName === 'push') {
+        tags += ` -t ${dockerImage}:${github.context.sha}`;
+    }
+
+
+    /* TODO Version */
+}
+
+async function addNugetConfig() {
+    console.log('Add nuget.config');
+    await exec.exec('dotnet new nugetconfig -o /tmp');
+}
+
+async function ensureMyGetNuGetSource(myGetNuGetSource) {
+    console.log('Ensure MyGet NuGet Source');
+    if(myGetNuGetSource) {
+        await exec.exec(`dotnet nuget add source "${myGetNuGetSource}" -n myget --configfile /tmp/nuget.config`)
+    }
+}
+
+run().then(_ => {});
